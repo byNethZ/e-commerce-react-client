ee31975d4b0e3c7c05a6c3a175169403cec4b0f3
NethZ <bynethz@gmail.com>
2023-10-21 18:43:26 -0500

    first commit

---
+++ src/data.js
@@ -0,0 +1,79 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "/img/reading-3723751_1920.jpg",
+      title: "Promociones por inaguración",
+      desc: "No te quedes sin aprovechar estas promociones de inaguración",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "/img/cakes-3257019_1280.jpg",
+      title: "Deliciosos Postres",
+      desc: "Todos nuestros postres están hechos con amor",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "/img/289123547_115773987832968_3264757421890590279_n.jpg",
+      title: "Deliciosos Postres",
+      desc: "Todos nuestros postres están hechos con amor",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "/img/cookies.jpg",
+      title: "Galletas",
+      cat:"cookies"
+    },
+    {
+      id: 2,
+      img: "img/pasteles.jpg",
+      title: "Pasteles",
+      cat:"pasteles"
+    },
+    {
+      id: 3,
+      img: "/img/frappe.jpg",
+      title: "Frappes",
+      cat:"frappes"
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"/img/306157451_127394453377960_914612527347540777_n.jpg",
+    },
+    {
+      id:2,
+      img:"/img/cookies-1264263_1920.jpg",
+    },
+    {
+      id:3,
+      img:"/img/ice-cream-3762671_1280.jpg",
+    },
+    {
+      id:4,
+      img:"/img/dessert-4071142_1280.jpg",
+    },
+    {
+      id:5,
+      img:"/img/cakes-3257019_1280.jpg",
+    },
+    {
+      id:6,
+      img:"/img/306157451_127394453377960_914612527347540777_n.jpg",
+    },
+    {
+      id:7,
+      img:"/img/cookies-1264263_1920.jpg",
+    },
+    {
+      id:8,
+      img:"/img/cookies-1264263_1920.jpg",
+    },
+  ]
